6f8f36d2f533a76f621c89c0c14078b844e6b361
codewithcodyy <131768877+codewithcodyy@users.noreply.github.com>
2023-05-20 00:04:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ menuIcon.onclick = () => {
  
 //scroll sections
 let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let navlinks = document.querySelectorAll('header nav a'); 
 
 window.onscroll = () => {
     sections.forEach(sec => {
